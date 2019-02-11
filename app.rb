50914114d925ba2615432caa04453eec7959c0eb
EC2 Default User <ec2-user@ip-172-31-46-229.eu-west-1.compute.internal>
2019-02-11 02:45:20 +0000

    working on passing tests

--- app.rb
+++ app.rb
@@ -39,9 +39,16 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
-    redirect '/show'
+  if /^[A-Z]+$/i.match(letter)
+    if !@game.guess(letter)
+       flash[:message] = "You have already used that letter."
+    end
+  else
+    flash[:message] = "Invalid guess."
   end
+  redirect '/show'
+  end
+  
   
   
   # Everytime a guess is made, we should eventually end up at this route.
@@ -63,5 +70,5 @@ class HangpersonApp < Sinatra::Base
     ### YOUR CODE HERE ###
     erb :lose # You may change/remove this line
   end
-  
+
 end
